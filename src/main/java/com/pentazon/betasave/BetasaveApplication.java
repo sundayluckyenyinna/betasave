2ebe7b0efa5b91871922da436ce2841829cfc796
unknown <TENECE@HQ-TEN-ICT-202.Accion.local>
2023-04-10 17:33:09 +0100

    Initial commit

---
+++ src/main/java/com/pentazon/betasave/BetasaveApplication.java
@@ -0,0 +1,13 @@
+package com.pentazon.betasave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetasaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetasaveApplication.class, args);
+	}
+
+}
