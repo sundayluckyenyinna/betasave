7c23b1f5230ade1be73c47f455d0985d3705aa0e
Olasubomi <shubbynlaa@gmail.com>
2023-04-10 19:08:16 +0100

    Innitial commit

--- src/main/java/com/pentazon/betasave/BetasaveApplication.java
+++ src/main/java/com/pentazon/betasave/BetasaveApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BetasaveApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(BetasaveApplication.class, args);
+		SpringApplication.run(BetasaveApplication.class, args
+		);
 	}
-
 }
