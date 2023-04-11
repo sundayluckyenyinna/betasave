02471fb717836b96e8361971e77b1155abb0126b
Olasubomi <shubbynlaa@gmail.com>
2023-04-11 02:18:31 +0100

    completed currency model.

--- src/main/java/com/pentazon/betasave/modules/user/model/BetasaveCurrency.java
+++ src/main/java/com/pentazon/betasave/modules/user/model/BetasaveCurrency.java
@@ -1,2 +1,27 @@
-package com.pentazon.betasave.modules.user.model;public class BetasaveCurrency {
+package com.pentazon.betasave.modules.user.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+@Entity
+@Table(name = "bt_currency")
+public class BetasaveCurrency {
+    @Id
+    private BigInteger id;
+
+    @Column(name = "currency_name")
+    private String currencyName;
+
+    @Column(name = "currency_symbol")
+    private String currencySymbol;
+
+    @Column(name = "usd_equivalent")
+    private BigDecimal usdEquivalent;
+
+    @Column(name = "status")
+    private String status;
 }
